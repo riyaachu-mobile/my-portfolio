b8e4d7ebbf2951b6b4a1f8813eeab8decbc18333
riyaachu-mobile <achuriya2@gmail.com>
2026-04-14 10:13:19 -0500

    updated web folder

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2181815317"
+    serviceWorkerVersion: "1473850642"
   }
 });
